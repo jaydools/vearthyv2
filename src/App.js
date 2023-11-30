eca7d563600c750f1b607ff7e10745374051644f
jaydools <134211868+jaydools@users.noreply.github.com>
2023-11-30 17:38:41 -0500

    basic structure of app complete, moving to branches to compartmentalize

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Homepage />} />
+                    <Route path="/sleep-health" element={<Bedroom />} />
+                    <Route path="/dental-health" element={<Bathroom />} />
+                    <Route path="/sustainable-design" element={<Bamboo />} />
+                    <Route path="/warranty" element={<Bamboo />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,51 @@
 import React from "react";
 import "./Header.scss";
+import { Link } from "react-router-dom";
 
 function Header() {
-    return <div>Header</div>;
+    return (
+        <nav className="navbar">
+            <div className="leftside">
+                <div className="leftside__logo">
+                    <Link to="/">
+                        <img src={logo} alt="logo" className="logo" />
+                    </Link>
+                </div>
+            </div>
+            <div className="middle">
+                <ul className="middle__list-parent">
+                    <li className="middle__list-item">
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li className="middle__list-item">
+                        <Link to="/sleep-health">Sleep Health</Link>
+                    </li>
+                    <li className="middle__list-item">
+                        <Link to="/dental-health">Dental Health</Link>
+                    </li>
+                    <li className="middle__list-item">
+                        <Link to="/sustainable-design">Sustainability</Link>
+                    </li>
+                    <li className="middle__list-item">
+                        <Link to="/warranty">Warranty</Link>
+                    </li>
+                </ul>
+                <ul className="rightside">
+                    <li className="rightside__list-item">
+                        <Link to="/cart">
+                            Cart
+                            <img src={cartStatic} alt="shopping cart" className="cart-static" />
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/login">
+                            <button>Log In</button>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 }
 
 export default Header;
