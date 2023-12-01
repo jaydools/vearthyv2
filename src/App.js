9df189f2e1feb821b2631489828493424a795f52
jaydools <134211868+jaydools@users.noreply.github.com>
2023-12-01 16:18:01 -0500

    responsive design implimented and finished
    at some point it might be good to add a shopping cart to beside the hamburg img

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
     return (
         <div className="App">
             <BrowserRouter>
+                <Headerhamburg />
                 <Header />
                 <Routes>
                     <Route path="/" element={<Homepage />} />
