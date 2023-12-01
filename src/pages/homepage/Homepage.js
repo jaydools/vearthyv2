5ae9e084bb0544572258e13585c974ec4727b8ce
jaydools <134211868+jaydools@users.noreply.github.com>
2023-12-01 17:39:47 -0500

    background hero image fixed for mobile to desktop

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "./Homepage.scss";
+import backdrop from "../../assets/hero.png";
 
 function Homepage() {
     return (
         <main>
-            <div className="container"></div>;
+            <div className="main-container">
+                <div className="hero-text">
+                    <h1 className="hero-text__header">Seo Title Here</h1>
+                    <h2 className="hero-text__subtext">More seo junk that describes the company</h2>
+                </div>
+            </div>
+            <img className="hero-backdrop" src={backdrop} alt="night sky"></img>
         </main>
     );
 }
