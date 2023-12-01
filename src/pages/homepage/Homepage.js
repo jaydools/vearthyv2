706e2d8ecd6a2cea00440cdf2d2312b28d7907c3
jaydools <134211868+jaydools@users.noreply.github.com>
2023-12-01 18:00:31 -0500

    bagan working on the hero, need ux inspiration

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -10,8 +10,22 @@ function Homepage() {
                     <h1 className="hero-text__header">Seo Title Here</h1>
                     <h2 className="hero-text__subtext">More seo junk that describes the company</h2>
                 </div>
+                <div className="hero-cards">
+                    <div className="hero-cards__one">
+                        <img className="hero-cards__img-one" /* src={}  */ alt="night sky"></img>
+                    </div>
+                    <div className="hero-cards__two">
+                        <img className="hero-cards__img-two" /* src={}  */ alt="night sky"></img>
+                    </div>
+                    <div className="hero-cards__three">
+                        <img className="hero-cards__img-three" /* src={} */ alt="night sky"></img>
+                    </div>
+                    <div className="hero-cards__four">
+                        <img className="hero-cards__img-four" /* src={} */ alt="night sky"></img>
+                    </div>
+                </div>
             </div>
-            <img className="hero-backdrop" src={backdrop} alt="night sky"></img>
+            <img className="hero-backdrop" src={backdrop} alt="green gradient background"></img>
         </main>
     );
 }
