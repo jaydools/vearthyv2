5f34600d87ae03fce37aa597727678ca04a388c4
jaydools <134211868+jaydools@users.noreply.github.com>
2023-12-01 15:56:26 -0500

    finished building hamburg menu and regular menu. Will next combine

--- src/components/headerHamburg/HeaderHamburg.js
+++ src/components/headerHamburg/HeaderHamburg.js
@@ -2,12 +2,13 @@ import React, { useState } from "react";
 import "./HeaderHamnurg.scss";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.svg";
+import { slide as Menu } from "react-burger-menu";
 
 function Header() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
+    const closeMenu = () => {
+        setIsMenuOpen(false);
     };
 
     return (
@@ -21,47 +22,36 @@ function Header() {
                     </div>
                 </div>
 
-                <div className={`navbar2__bottom ${isMenuOpen ? "open" : ""}`}>
-                    <ul className="middle2__list-parent">
-                        <li className="middle2__list-item">
-                            <Link to="/" className="middle2__link">
-                                Home
-                            </Link>
-                        </li>
-                        <li className="middle2__list-item">
-                            <Link to="/sleep-health" className="middle2__link">
-                                Sleep Health
-                            </Link>
-                        </li>
-                        <li className="middle2__list-item">
-                            <Link to="/dental-health" className="middle2__link">
-                                Dental Health
-                            </Link>
-                        </li>
-                        <li className="middle2__list-item">
-                            <Link to="/sustainable-design" className="middle2__link">
-                                Sustainability
-                            </Link>
-                        </li>
-                        <li className="middle2__list-item">
-                            <Link to="/warranty" className="middle2__link">
-                                Warranty
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
+                <Menu
+                    noOverlay
+                    right
+                    isOpen={isMenuOpen}
+                    onStateChange={({ isOpen }) => setIsMenuOpen(isOpen)}
+                    className="menu-container"
+                >
+                    <Link to="/" className="middle2__link" onClick={closeMenu}>
+                        Home
+                    </Link>
+                    <Link to="/sleep-health" className="middle2__link" onClick={closeMenu}>
+                        Sleep Health
+                    </Link>
+                    <Link to="/dental-health" className="middle2__link" onClick={closeMenu}>
+                        Dental Health
+                    </Link>
+                    <Link to="/sustainable-design" className="middle2__link" onClick={closeMenu}>
+                        Sustainability
+                    </Link>
+                    <Link to="/warranty" className="middle2__link" onClick={closeMenu}>
+                        Warranty
+                    </Link>
 
-                <div>
-                    <div className={`rightside2 ${isMenuOpen ? "open" : ""}`}>
-                        <Link to="/cart" className="rightside2__cart">
-                            Cart
-                            {/* <img src={cartStatic} alt="shopping cart" className="cart-static" /> */}
-                        </Link>
-                        <Link to="/login" className="rightside2__login">
-                            Log In
-                        </Link>
-                    </div>
-                </div>
+                    <Link to="/cart" className="rightside2__cart" onClick={closeMenu}>
+                        Cart
+                    </Link>
+                    <Link to="/login" className="rightside2__login" onClick={closeMenu}>
+                        Log In
+                    </Link>
+                </Menu>
             </nav>
         </span>
     );
