1c794abbd5071099defe1dc19dd02c61dd78ebd6
jaydools <134211868+jaydools@users.noreply.github.com>
2023-12-02 19:49:00 -0500

    basic outline and styling done for mobile

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./Homepage.scss";
 import backdrop from "../../assets/hero.png";
 import Hero from "../../components/hero/Hero";
+import SleepSection from "../../components/sleepSection/SleepSection";
 
 function Homepage() {
     return (
         <main>
             <Hero />
+            <SleepSection />
             <img className="hero-backdrop" src={backdrop} alt="green gradient background"></img>
         </main>
     );
